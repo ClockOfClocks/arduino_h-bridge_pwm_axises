d09d6d09d8359932213f044bd6aa341fd875b805
deanar <rdeanar@gmail.com>
2019-01-26 20:57:48 +0300

    Non-blocking steps without delays.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <math.h>
 
+#define MICROSTEPS 64
 
 int motorA1 = 11;
 int motorA2 = 12;
@@ -13,10 +14,18 @@ int enablePinB = 6;
 
 int fullStepsPerSecond;
 
+unsigned long delayMicroS;
+
 int sinMap[256];
 int cosMap[256];
 
-#define MICROSTEPS 64
+unsigned long previousMicros = 0;
+
+int oneDirectionMSteps = 180 * MICROSTEPS;
+
+bool directionCw = true;
+
+int currentMStep = 0;
 
 void setup()
 {
@@ -28,19 +37,25 @@ void setup()
         pinMode(enablePinA, OUTPUT);
         pinMode(enablePinB, OUTPUT);
 
-        fullStepsPerSecond = 30;
+        fullStepsPerSecond = 50;
 
         // this line is for Leonardo's, it delays the serial interface
         // until the terminal window is opened
         while (!Serial);
         Serial.begin(9600);
 
-        for(int i=0; i< MICROSTEPS * 4; i++){
+        for (int i = 0; i < MICROSTEPS * 4; i++)
+        {
                 sinMap[i] = round(sin(M_PI * i / (2 * MICROSTEPS)) * 255);
-                cosMap[i] = round(cos(M_PI * i / (2 * MICROSTEPS)) * 255);  
+                Serial.println(sinMap[i]);
+                cosMap[i] = round(cos(M_PI * i / (2 * MICROSTEPS)) * 255);
         }
+
+        delayMicroS = pow(10, 6) / (fullStepsPerSecond * MICROSTEPS);
 }
-void makeStep(int microstep){
+
+void makeStep(int microstep)
+{
         microstep = microstep % (MICROSTEPS * 4);
 
         int coilA = sinMap[microstep];
@@ -56,24 +71,23 @@ void makeStep(int microstep){
         digitalWrite(motorB2, coilB >= 0 ? LOW : HIGH);
 }
 
-
 void loop()
 {
-        int s = 180 * MICROSTEPS;
-        uint64_t delayMicroS = pow(10, 6) / (fullStepsPerSecond * MICROSTEPS);
-        //delayMicroS = pow(10, 6);
+        unsigned long currentMicros = micros();
 
-        for (int i = 0; i < s; i++) {
-                makeStep(i);
-                delayMicroseconds(delayMicroS);
+        if (currentMicros - previousMicros >= delayMicroS)
+        {
+                previousMicros = currentMicros;
+
+                if (currentMStep >= oneDirectionMSteps)
+                {
+                        directionCw = false;
+                }
+                else if (currentMStep < 0)
+                {
+                        directionCw = true;
+                }
+
+                makeStep(directionCw ? ++currentMStep : --currentMStep);
         }
-
-        delay(4000);
-
-        for (int i = s; i >= 0; i--) {
-                makeStep(i);
-                delayMicroseconds(delayMicroS);
-        }
-
-        delay(4000);
 }
